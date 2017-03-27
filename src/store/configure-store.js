ab14cd2246e96149c3e094fda621bb305352cc1c
pesehr <sepehr.sabour@gmail.com>
2017-03-27 13:14:26 +0430

    redux-logger bug fixed

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -2,7 +2,7 @@
 
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
-import createLogger from "redux-logger";
+import { createLogger } from "redux-logger";
 import rootReducer from "../reducers";
 
 const loggerMiddleware = createLogger();
